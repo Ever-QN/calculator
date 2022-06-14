39d14ac67e06b017757f5710bdc1e2b9036d6e38
Ever-QN <QuynhTNgo@live.com>
2022-06-14 15:15:09 -0600

    Added operator function Linked CSS and JS files to index.html

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-function add (a, b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
-function divide (a, b) {
+function divide(a, b) {
     return a / b;
 }
 
+function operate(operator, firstNumber, secondNumber) {
+    switch(operator) {
+        case 'addition':
+            return add(firstNumber, secondNumber);
+        case 'subtraction':
+            return subtract(firstNumber, secondNumber);
+        case 'multiplication':
+            return multiply(firstNumber, secondNumber);
+        case 'division':
+            return divide(firstNumber, secondNumber);
+        default:
+            return 'ERROR';
+    }
+
+}
